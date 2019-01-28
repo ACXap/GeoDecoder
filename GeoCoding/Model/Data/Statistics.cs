9ac50ad11feb5442ab6af31caffd9c2898af4c89
ACXap <acxap@list.ru>
2019-01-28 13:51:20 +0700

    Update Statistics.cs
    Добавлены комментарии

--- GeoCoding/Model/Data/Statistics.cs
+++ GeoCoding/Model/Data/Statistics.cs
@@ -1,13 +1,37 @@
 ﻿namespace GeoCoding
 {
+  /// <summary>
+  /// Класс для хранения статистики
+  /// </summary>
     public class Statistics
     {
+      /// <summary>
+      /// Всего объектов в коллекции
+      /// </summary>
         public int AllEntity { get; set; }
+        /// <summary>
+        /// Всего объектов со статусом "ОК"
+        /// </summary>
         public int OK { get; set; }
+        /// <summary>
+        /// Всего объектов со статусом "Ошибка"
+        /// </summary>
         public int Error { get; set; }
+        /// <summary>
+        /// Всего объектов со статусом "Негеокодирован"
+        /// </summary>
         public int NotGeoCoding { get; set; }
+        /// <summary>
+        /// Всего объектов со статусом "Геокодируется сейчас"
+        /// </summary>
         public int GeoCodingNow { get; set; }
+        /// <summary>
+        /// Всего объектов с типом объекта "Дом"
+        /// </summary>
         public int House { get; set; }
+        /// <summary>
+        /// Всего объектов с качеством геокодирования "Точное геокодирование"
+        /// </summary>
         public int Exact { get; set; }
     }
 }
