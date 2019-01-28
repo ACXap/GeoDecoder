8e8c8ff974cbabce34b04ec4a609b13380502892
ACXap <acxap@list.ru>
2019-01-28 13:53:05 +0700

    Update StatusType.cs
    Добавлены комментарии

--- GeoCoding/Model/Data/StatusType.cs
+++ GeoCoding/Model/Data/StatusType.cs
@@ -1,10 +1,25 @@
 ﻿namespace GeoCoding
 {
+  /// <summary>
+  /// Перечисление статусов геокодирования
+  /// </summary>
     public enum StatusType
     {
+      /// <summary>
+      /// Негеокодирован
+      /// </summary>
         NotGeoCoding,
+        /// <summary>
+        /// Геокодирован без ошибок
+        /// </summary>
         OK,
+        /// <summary>
+        /// Геокодирован с ошибками
+        /// </summary>
         Error,
+        /// <summary>
+        /// Геокодируется в данный момент
+        /// </summary>
         GeoCodingNow
     }
 }
