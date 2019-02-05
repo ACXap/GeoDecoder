6df06b8ef86731f305b8543358ced5b26a3cb136
ACXap <acxap@list.ru>
2019-02-05 14:28:44 +0700

    Добавлены юзер-контролы для настроек и sql-запроса
    Немного порядка

---
+++ GeoCoding/UserControls/FlyoutSettings.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GeoCoding.UserControls
+{
+    public partial class FlyoutSettings : UserControl
+    {
+        public FlyoutSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GeoCoding/UserControls/FlyoutSql.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GeoCoding.UserControls
+{
+    public partial class FlyoutSql : UserControl
+    {
+        public FlyoutSql()
+        {
+            InitializeComponent();
+        }
+    }
+}
