7d658ae1c8c18e92f934fa67a5b7c8b6dca7b8f7
ACXap <acxap@list.ru>
2019-02-07 16:22:27 +0700

    Множество геокодеров

---
+++ GeoCoding.GeoCodingService/Data/GeoService.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoCoding.GeoCodingService
+{
+    public class GeoService
+    {
+        public string Name { get; set; }
+        public IGeoCodingService Service { get; set; }
+    }
+}
---
+++ GeoCoding.GeoCodingService/MainGeoService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GeoCoding.GeoCodingService
+{
+    public static class MainGeoService
+    {
+        public static IEnumerable<GeoService> GetAllService()
+        {
+            return new List<GeoService>()
+            {
+                new GeoService()
+                {
+                    Name = "Yandex",
+                    Service = new YandexGeoCodingService()
+                },
+                new GeoService()
+                {
+                    Name = "Sputnik",
+                    Service = new SputnikGeoCodingService()
+                }
+            };
+        }
+    }
+}
