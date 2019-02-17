ef3f719ae0448d1ce16fd1cf999bd432a9749d8a
Aleksandr Kharlamov <acxap@list.ru>
2019-02-17 18:00:32 +0700

    Юзерконтрол процес обработки данных

---
+++ GeoCoding/UserControls/WorkIndication.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GeoCoding.UserControls
+{
+    public partial class WorkIndication : UserControl
+    {
+        public WorkIndication()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(WorkIndication), new PropertyMetadata(null));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(WorkIndication), new PropertyMetadata(false));
+
+        public Brush TextColor
+        {
+            get { return (Brush )GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(Brush ), typeof(WorkIndication), new PropertyMetadata(Brushes.Red));
+    }
+}
