3c6c52704c96b633a92c6cf9d1bd9394c8d81b97
ACXap <acxap@list.ru>
2019-02-21 09:29:06 +0700

    Добавлен прогресбар на таскбар

---
+++ GeoCoding/Converters/ConverterStartGeoToProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Shell;
+
+namespace GeoCoding
+{
+    public class ConverterStartGeoToProgress : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if ((bool)value)
+                {
+                    return TaskbarItemProgressState.Normal;
+                }
+            }
+            return TaskbarItemProgressState.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
