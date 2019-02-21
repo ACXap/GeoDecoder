bf1082d7c4b047219533500f59ab0d06baed9e15
ACXap <acxap@list.ru>
2019-02-21 13:41:27 +0700

    Чуток порядка

--- GeoCoding/Converters/ConverterStatGeoToProgressState.cs
+++ GeoCoding/Converters/ConverterStatGeoToProgressState.cs
@@ -5,18 +5,14 @@ using System.Windows.Shell;
 
 namespace GeoCoding
 {
-    public class ConverterStatGeoToProgress : IValueConverter
+    /// <summary>
+    /// Класс конвертер запущенного процесса в состояние индикатора выполнения на панели задач
+    /// </summary>
+    public class ConverterStatGeoToProgressState : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value != null)
-            {
-                if ((bool)value)
-                {
-                    return TaskbarItemProgressState.Normal;
-                }
-            }
-            return TaskbarItemProgressState.None;
+            return (value != null) && (bool)value ? TaskbarItemProgressState.Normal : TaskbarItemProgressState.None;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- GeoCoding/Converters/ConverterStatToProgress.cs
+++ GeoCoding/Converters/ConverterStatToProgress.cs
@@ -4,15 +4,14 @@ using System.Windows.Data;
 
 namespace GeoCoding
 {
+    /// <summary>
+    /// Класс конвертер процентов выполненной работы в значение от 0 до 1
+    /// </summary>
     public class ConverterStatToProgress : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value != null)
-            {
-                return ((double)value) / 100.0f;
-            }
-            return 0;
+            return value != null ? ((double)value) / 100.0 : 0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
