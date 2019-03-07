c12e034978ff1d3e11a5da88dab7bda7b78fedbd
Aleksandr Kharlamov <acxap@list.ru>
2019-03-07 18:29:17 +0700

    Размер окна пытается под размеры экрана Чуток минимилизация интерфейса

--- GeoCoding/App.xaml.cs
+++ GeoCoding/App.xaml.cs
@@ -9,5 +9,25 @@ namespace GeoCoding
         {
             DispatcherHelper.Initialize();
         }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow win = new MainWindow
+            {
+                Height = GetHeight(),
+                Width = GetWidth()
+            };
+            win.Show();
+        }
+
+        private double GetHeight()
+        {
+            return SystemParameters.PrimaryScreenHeight * 0.60;
+        }
+
+        private double GetWidth()
+        {
+            return SystemParameters.PrimaryScreenWidth * 0.55;
+        }
     }
 }
