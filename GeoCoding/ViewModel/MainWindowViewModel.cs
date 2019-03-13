7d3d2a263d527026ea836f90806ad5b8b44affdf
ACXap <acxap@list.ru>
2019-03-13 09:47:49 +0700

    Заголовок отображает процент
    Кнопки уменьшены

--- GeoCoding/ViewModel/MainWindowViewModel.cs
+++ GeoCoding/ViewModel/MainWindowViewModel.cs
@@ -624,7 +624,7 @@ namespace GeoCoding
                                 }
                             }
                         }
-                    }));
+                    }, obj=> !_isStartGeoCoding));
 
         /// <summary>
         /// Команда для сохранения настроек
